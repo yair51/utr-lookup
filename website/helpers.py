4381f4ed5ec92895155189758230dbd15b951c2d
Yair Gritzman <yairgritzman@gmail.com>
2022-08-10 20:50:21 -0400

    accesses cookies file to authenticate with UTR

--- website/helpers.py
+++ website/helpers.py
@@ -1,43 +1,69 @@
 import requests
 import os
+from os.path import exists as file_exists
 from dotenv import load_dotenv
 import pickle
 load_dotenv()
+
 
 def utr_login():
     """Log in to UTR"""
 
     # Contact API
-    data = {"email": os.getenv("UTR_EMAIL"), "password": os.getenv("UTR_PASSWORD")}
+    data = {"email": os.getenv("UTR_EMAIL"),
+            "password": os.getenv("UTR_PASSWORD")}
     s = requests.Session()
-    r = s.post('https://app.universaltennis.com/api/v1/auth/login',json=data)
+    r = s.post('https://app.universaltennis.com/api/v1/auth/login', json=data)
 
     # Store cookies in external file
     with open('cookies.pickle', 'wb') as f:
         pickle.dump(s.cookies, f)
-        print("done")
 
 
 def utr_search(name):
     """Search UTR of given player"""
+
+    # If 'cookies.pickle' file doesn't exist, authenticate with UTR
+    if not file_exists('cookies.pickle'):
+        utr_login()
 
     # Fetch cookies from external file
     with open('cookies.pickle', 'rb') as f:
         cookies = pickle.load(f)
 
     # Contact API
-    query = {"query": name}
+    query = {"query": name, "top": 10}
     try:
-        r = requests.get('https://api.universaltennis.com/v2/search/players', data=query, cookies=cookies)
+        r = requests.get(
+            'https://api.universaltennis.com/v2/search/players', data=query, cookies=cookies)
         r.raise_for_status()
     except requests.RequestException as err:
         print(err)
         return None
 
     # Parse response
+    try:
+        players = []
+        utr = r.json()
+        for count, player in enumerate(utr["hits"]):
+            players.append({
+                "name": player["source"]["displayName"],
+                "singlesUtr": player["source"]["singlesUtrDisplay"],
+                "singlesRatingAccuracy": int(player["source"]["ratingProgressSingles"]),
+                "doublesUtr": player["source"]["doublesUtrDisplay"],
+                "doublesRatingAccuracy": int(player["source"]["ratingProgressDoubles"]),
+                "threeMonthRating": player["source"]["threeMonthRatingChangeDetails"]["ratingDisplay"]
+            })
+        return players
+    except (KeyError, TypeError, ValueError):
+        return None
 
-    utr = r.json()
-    for i in range(10):
-        print(utr["hits"][i]["source"]["singlesUtr"])
 
-utr_login()
+def is_authenticated(players):
+    """Check if already authenticated with UTR. Returns True if logged in."""
+
+    # Check JSON response for login status
+    for player in players:
+        if ("x" in player["singlesUtr"] or "x" in player["doublesUtr"]):
+            return False
+    return True
--- website/views.py
+++ website/views.py
@@ -1,15 +1,35 @@
 
 
 from flask import Blueprint, render_template, request, flash, jsonify, redirect, url_for
+from .helpers import is_authenticated, utr_login, utr_search
 
 
 views = Blueprint('views', __name__)
 
+
 @views.route('/', methods=['GET'])
 def home():
+    """Displays home screen"""
     return render_template("index.html", title="Home")
 
 
 @views.route('/result')
 def result():
+    """Displays search results"""
     return render_template("result.html", title="Result")
+
+
+@views.route('/search')
+def search():
+    """calls UTR API to search player"""
+
+    # Submit search query to UTR
+    query = request.args.get("query")
+    results = utr_search(query)
+
+    # Login to UTR and rerun search query if not authenticated
+    if not is_authenticated(results):
+        utr_login()
+        results = utr_search(query)
+
+    return render_template("result.html", results=results)
